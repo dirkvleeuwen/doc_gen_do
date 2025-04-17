020accdd59e35f05b50cec312c8b29765d518964
Dirk van Leeuwen <gitdjangoproject@3838.eu>
2025-04-18 00:19:59 +0200

    static_root toegevoegd aan settings.py voor collectstatic in production

--- instrument_generator/settings.py
+++ instrument_generator/settings.py
@@ -128,7 +128,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# De map waar 'collectstatic' alle static files verzamelt voor productie
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
