3cb465ffdbb7ce2521ee5fc5199a85a524225e50
Dirk van Leeuwen <gitdjangoproject@3838.eu>
2025-04-20 01:20:39 +0200

    Source .env in migrate_and_collectstatic.sh

--- scripts/migrate_and_collectstatic.sh
+++ scripts/migrate_and_collectstatic.sh
@@ -1,5 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Ga naar project-dir
 cd /home/ubuntu/instrument_generator
+
+# Exporteer alle vars uit .env
+set -o allexport
+source .env
+set +o allexport
+
+# Activeer virtuele omgeving
 source venv/bin/activate
+
+# Migrate & collectstatic
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
