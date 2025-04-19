43954c56cd927154e21f2852f930e220d60f26bc
Ubuntu <ubuntu@ip-172-31-22-47.eu-central-1.compute.internal>
2025-04-19 18:13:50 +0000

    Use @login_not_required on health endpoint

--- instrument_generator/health.py
+++ instrument_generator/health.py
@@ -1,6 +1,8 @@
 # instrument_generator/health.py
 
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_not_required
 
+@login_not_required
 def health(request):
     return HttpResponse("OK")
